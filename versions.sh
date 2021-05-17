6529c9eff2280207e50346a84a7ec1579ee4b2b5
John Kenny <64958468+johnkennyuk@users.noreply.github.com>
2021-05-17 11:29:25 +0100

    Initialize v3

---
+++ versions.sh
@@ -0,0 +1,10 @@
+8.5.4
+v8.5base
+###########################-VERSIONS-PRIOR
+8.5.0
+###### DONT RECOMMEND BELOW (ARCHIEVED VERSIONS) 
+8.4.20
+8.4.18
+8.3.17
+###########################-COMMANDS-BELOW-FOR-TESTING
+v8.4base
